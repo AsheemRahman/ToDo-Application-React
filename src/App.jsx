2ce65e456402f0e44a75cdfd74dd62b1790193c0
Asheem Rahman <mhdasheem@gmail.com>
2024-10-28 10:48:50 +0530

    Delete images

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Todo from './Components/Todo'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Todo />
